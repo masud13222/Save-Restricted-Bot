1d3b69a4251ab95e04d7fa3984b0b9e125c40c0a
masud132213 <masud132213@gmail.com>
2024-12-29 23:38:00 +0600

    first commit

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+
+# Bot token @Botfather
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5767231785:AAHaszuB2epfI9oe9bLwgM8x5vpqMn5tmYA")
+
+# Your API ID from my.telegram.org
+API_ID = int(os.environ.get("API_ID", "5587539"))
+
+# Your API Hash from my.telegram.org
+API_HASH = os.environ.get("API_HASH", "8670b598fef377e6782429b1f664dce6")
+
+# Your Owner / Admin Id For Broadcast
+ADMINS = list(map(int, os.environ.get("ADMINS", "1826754085").split()))
+
+# Your Mongodb Database Url
+# Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://cinemazbd:A3cyYdaS3JkOGYog@cluster0.akey0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
+
+# If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
+ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
+
+# Verification duration in seconds (default is 6 hours)
+VERIFICATION_DURATION = int(os.environ.get("VERIFICATION_DURATION", 21600))  # 6 hours in seconds
